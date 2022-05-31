f4d9a836a0ddc2d60dbae31ce50da443084b9511
dhruvraj <dhruvrajsinghsolanki161@gmail.com>
2022-05-31 13:02:51 +0530

    fix: landing page responsive

--- components/Landing.tsx
+++ components/Landing.tsx
@@ -1,20 +1,24 @@
 import Image from "next/image";
+import React from "react";
 
 export const Landing = () => {
   return (
     <>
-      <div className="relative min-h-screen  overflow-hidden">
-        <div className="bg-[#F1ACAE] blur-[300px] -top-64 -right-16 rounded-full absolute w-[30rem] h-[30rem] "></div>
-        <main className=" bgTexture bg-[#0A1837] flex flex-col justify-center items-center space-y-20 min-h-screen ">
-          <h1 className="text-3xl xl:text-6xl font-extrabold uppercase super font-Lexend ">
+      <div className="bgLanding md:grid md:grid-cols-2 ">
+        <div></div>
+        <main className="  flex flex-col justify-center items-center space-y-20 min-h-screen  ">
+          <h1 className="text-4xl xl:text-5xl 2xl:text-6xl font-extrabold uppercase super font-Lexend ">
             Supercollabs
           </h1>
-          <div className="flex flex-col justify-center text-center">
-            <p className="text-transparent bg-clip-text text-xl xl:text-2xl font-medium capitalize superdes font-Lexend">
+          <div className="flex flex-col justify-center items-center text-center space-y-2">
+            <p className="text-transparent bg-clip-text text-xl xl:text-2xl 2xl:text-3xl tracking-tighter font-medium capitalize superdes font-Lexend">
               Reward Your Team With A Proof Of
             </p>
-            <p className="text-transparent bg-clip-text text-xl xl:text-2xl font-medium capitalize superdes font-Lexend">
-              Collaboration As An NFT 🏆
+            <p className=" text-xl 2xl:text-3xl xl:text-xl font-medium capitalize  font-Lexend flex flex-row gap-x-3 ">
+              <span className="text-transparent bg-clip-text superdes tracking-tighter ">
+                Collaboration As An NFT
+              </span>
+              🏆
             </p>
           </div>
           <button className="bg-phan p-5 h-12 w-72 gap-x-4 relative flex justify-center items-center rounded-2xl font-medium text-lg font-Outfit text-white ">
@@ -27,7 +31,6 @@ export const Landing = () => {
             Connect Wallet
           </button>
         </main>
-        <div className="bg-[#50508D] blur-[300px] -bottom-64 -left-16 rounded-full absolute w-[30rem] h-[30rem] "></div>
       </div>
     </>
   );
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,21 +1,11 @@
 import type { NextPage } from "next";
-import Image from "next/image";
+
 import { Landing } from "../components/Landing";
-import { Test } from "../components/test";
 
 const Home: NextPage = () => {
   return (
-    // <>
-    //   <div className="flex  ">
-    //     {/* <Image src='/gradient1.png' alt="gradient"  /> */}
-    //     <div className="bglanding"></div>
-    //     <div className="w-full">
-    //       <Landing />
-    //     </div>
-    //   </div>
-    // </>
     <>
-      <Test />
+      <Landing />
     </>
   );
 };
